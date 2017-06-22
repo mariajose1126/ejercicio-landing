7e7977344e0dbef1580f8b7a31c6f03bd8045c59
mariajose1126 <mariajose.olvera1126@gmail.com>
2017-06-21 20:26:18 -0500

    Agrega contenido de examen front

---
+++ prueba1.js
@@ -0,0 +1,32 @@
+function buscadorNumeros(){
+  var number = 0;
+  var n;
+  var primo = 0;
+  for(i = 0; i <= 9; i++){
+    number += 1;
+    if (number%i == 0){
+      console.log(number + " es par");
+    } else if (number%2 != 0){
+      console.log(number + " es impar");
+    }
+  }
+}
+
+buscadorNumeros();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
